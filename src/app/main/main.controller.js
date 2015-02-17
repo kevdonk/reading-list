54be6989d10186b37a525f2536846ccbaa155c6c
Kevin Mayo <kevin@mayoind.com>
2015-02-17 13:51:13 -0700

    load bookmarks from json

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,19 +1,8 @@
 'use strict';
 
 angular.module('readingList')
-  .controller('MainCtrl', function ($scope) {
-    $scope.bookmarks = [
-      {
-        'name': 'Cmder',
-        'url': 'http://bliker.github.io/cmder',
-        'desc': 'Windows Console Emulator',
-        'category': 'Tools'
-      },
-      {
-        'name': 'Yeoman',
-        'url': 'http://yeoman.io',
-        'desc': 'WebApp Scaffolding Tool',
-        'category': 'Tools'
-      }
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get("../../assets/data/bookmarks.json").success(function(data) {
+      $scope.bookmarks = data;
+    });
   });
